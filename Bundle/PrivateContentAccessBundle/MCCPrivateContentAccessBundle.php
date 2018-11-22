94c49982420def9448df8a248f9dafd5e7d1309e
mbouhlel75 <m.bouhlel@novactive.com>
2018-11-22 16:59:41 +0100

    fix error

--- Bundle/PrivateContentAccessBundle/MCCPrivateContentAccessBundle.php
+++ Bundle/PrivateContentAccessBundle/MCCPrivateContentAccessBundle.php
@@ -22,4 +22,22 @@ class MCCPrivateContentAccessBundle extends Bundle
 
         return $this->extension;
     }
+
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ContentHelperRegistryPass());
+        $container->addCompilerPass(new LocationHelperRegistryPass());
+    }
 }
